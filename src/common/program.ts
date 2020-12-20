de91f1ed130ac1796c75ee1a49bad93f19466eaf
Guy Marino <gxm262@case.edu>
2020-12-20 18:38:47 -0500

    Update to read binary program

--- src/common/program.ts
+++ src/common/program.ts
@@ -1,43 +1,93 @@
+import { assert } from "console";
 
 const fs = require('fs')
 
 class Program {
 
-    address: number;
-    program: number[];
+    private address: number;
+    private program: number[];
 
-    constructor() {
-        this.address = 0x00
-        this.program = []
+
+    constructor(){
+        this.address = 0x00;
+        this.program = [];
     }
 
-    setupFile(filepath){
 
+    public setupFile(filepath: string){
+        this.clearProgram();
+        this.reset();
+
+        this.readFile(filepath);
     }
 
-    setupTestProgram(testProgram){
 
+    public setupTestProgram(testProgram: number[]){
+        this.clearProgram();
+        this.reset();
+
+        this.program = testProgram;
     }
+
+
+    // Clear the current program
+    public clearProgram(){
+        this.program = [];
+    }
+
+
+    public reset(){
+        this.address = 0x00;
+    }
+
+
+    public setAddress(newAddress: number){
+        this.address = newAddress;
+    }
+
+
+    public getBytes(numBytes: number): number[]{
+        var output: number[] = [];
+        var stopPoint: number = this.address + numBytes;
+
+        if(stopPoint <= this.address){
+            throw new Error('Number of bytes to read cannot be negative')
+        }
+        if(stopPoint >= this.program.length){
+            throw new RangeError('Requesting information beyond program bounds')
+        }
+
+        for(var i = this.address; i < stopPoint; i++){
+            this.address++;
+            output.push(this.program[i]);
+        }
+
+        return output;
+    }
+
 
     // Read the raw binary from a source file
-    readFile(filepath) {
-        fs.open(filepath, 'r', (err, fd) => {
+    private readFile(filepath: string) {
+        this.clearProgram();
+
+        fs.open(filepath, 'r', (err: Error, fd: number) => {
             if(err) {
-                console.error(err)
-                process.exit(1)
+                console.error(err);
+                process.exit(1);
             }
 
-            fs.read(fd, (err, read, buffer) => {
+            fs.read(fd, (err: Error, read: number, buffer: Buffer) => {
                 if(err){
-                    console.error(err)
+                    console.error(err);
                     process.exit(1);
                 }
 
                 for(var i = 0; i < read; i++){
-                    
+                    var byte: number = buffer.readUInt8()
+                    this.program.push(byte);
                 }
-            })
-        })
+            });
+        });
     }
 }
 
