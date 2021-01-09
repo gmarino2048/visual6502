97c8875b7438402763b0ba2e3a6f5595a7e3391e
Guy Marino <gxm262@case.edu>
2021-01-09 13:52:16 -0500

    Add common configuration class

--- src/common/common.ts
+++ src/common/common.ts
@@ -2,3 +2,21 @@
  * This is the common file, designed to hold all the persistant
  * data throughout the runtime of the program.
  */
+
+import { Program } from './program'
+
+export class Config{
+    public chip: string
+    public program: Program
+
+    public transdefs: any[]
+    public segdefs: any[]
+
+    constructor(){
+        this.chip = null
+        this.program = null
+        
+        this.transdefs = []
+        this.segdefs = []
+    }
+}
